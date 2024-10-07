ef82995f4f44b965abaa7831a0a0ca87b80f7b2f
soomni01 <dsgh8933@naver.com>
2024-10-07 17:33:13 +0900

    javaBeans 사용

---
+++ src/main/java/com/example/spring/dto/c24/Customer.java
@@ -0,0 +1,10 @@
+package com.example.spring.dto.c24;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String name;
+    private String contactName;
+    private String address;
+}

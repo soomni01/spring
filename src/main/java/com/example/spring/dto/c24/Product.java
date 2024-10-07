e9e914046aba4a9af627d462c2b411e06f41f8ca
soomni01 <dsgh8933@naver.com>
2024-10-07 17:32:52 +0900

    상품목록 조회

---
+++ src/main/java/com/example/spring/dto/c24/Product.java
@@ -0,0 +1,11 @@
+package com.example.spring.dto.c24;
+
+import lombok.Data;
+
+@Data
+public class Product {
+    private String id;
+    private String name;
+    private String unit;
+    private String price;
+}

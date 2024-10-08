8d3a6f48badc17fb3b16b093f5e46793cb862cf0
soomni01 <dsgh8933@naver.com>
2024-10-08 09:35:08 +0900

    select

---
+++ sql/01select.sql
@@ -0,0 +1,38 @@
+USE w3schools; # 실행 ctrl enter
+
+SELECT *
+FROM Customers;
+SELECT *
+FROM Employees;
+SELECT *
+FROM Products;
+SELECT *
+FROM Categories;
+
+# Data들은 Table에 저장됨
+# Table은 행과 열로 구성됨
+
+# 행(row) : record
+# 열(column) : attribute, field
+
+# SELECT : 테이블의 데이터를 조회(가져옴)
+# SELECT * FROM 테이블명;
+SELECT *
+FROM Products;
+
+SELECT *
+FROM Categories;
+SELECT *
+FROM Customers;
+SELECT *
+FROM Employees;
+SELECT *
+FROM OrderDetails;
+SELECT *
+FROM Orders;
+SELECT *
+FROM Products;
+SELECT *
+FROM Shippers;
+SELECT *
+FROM Suppliers;

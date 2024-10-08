794872a06aea9046b8dfca55d23e6dc89ef0c3af
soomni01 <dsgh8933@naver.com>
2024-10-08 17:15:31 +0900

    고객 조회

---
+++ src/main/java/com/example/spring/dto/c26/Customer.java
@@ -0,0 +1,14 @@
+package com.example.spring.dto.c26;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String id;
+    private String name;
+    private String contact;
+    private String address;
+    private String city;
+    private String postalCode;
+    private String country;
+}

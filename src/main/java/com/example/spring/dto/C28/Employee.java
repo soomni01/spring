1dcad75c78f2e380b004611c159332b4e4bca3aa
soomni01 <dsgh8933@naver.com>
2024-10-11 16:36:58 +0900

    직원 조회

---
+++ src/main/java/com/example/spring/dto/C28/Employee.java
@@ -0,0 +1,13 @@
+package com.example.spring.dto.C28;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String id;
+    private String lastName;
+    private String firstName;
+    private String birthDate;
+    private String photo;
+    private String notes;
+}

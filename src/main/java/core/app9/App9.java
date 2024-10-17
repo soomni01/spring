b8b8f25816ccb1f6adeac090af735cfd1eb10e0b
soomni01 <dsgh8933@naver.com>
2024-10-17 16:43:33 +0900

    생성자 주입

--- src/main/java/core/app9/App9.java
+++ src/main/java/core/app9/App9.java
@@ -3,15 +3,14 @@ package core.app9;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class App9 {
     public static void main(String[] args) {
         BeanFactory context = SpringApplication.run(App9.class, args);
-        Com1 b1 = context.getBean(Com1.class);
-        Com2 b2 = context.getBean(Com2.class);
+        Comp1 b1 = context.getBean(Comp1.class);
+        Comp2 b2 = context.getBean(Comp2.class);
 
-        System.out.println(b1.getCom2() == b2); // true
+        System.out.println(b1.getComp2() == b2); // true
     }
 }

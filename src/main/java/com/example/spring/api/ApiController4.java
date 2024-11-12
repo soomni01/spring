3c014fdfe1cdb72571d17452ceb72622df96f9f5
soomni01 <dsgh8933@naver.com>
2024-11-12 11:35:01 +0900

    pathVariable

--- src/main/java/com/example/spring/api/ApiController4.java
+++ src/main/java/com/example/spring/api/ApiController4.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/main4")
 public class ApiController4 {
+
+    @GetMapping("sub3/{address}")
+    public void sub3(@PathVariable String address) {
+        System.out.println("address = " + address);
+    }
+
     @GetMapping("sub2/{quantity}")
     public void sub2(@PathVariable Integer quantity) {
         System.out.println("quantity = " + quantity);

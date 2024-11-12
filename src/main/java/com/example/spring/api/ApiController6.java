b4d0e9830d6a9e716cbcaf7b0c43f35612b0e762
soomni01 <dsgh8933@naver.com>
2024-11-12 16:26:21 +0900

    RestController

--- src/main/java/com/example/spring/api/ApiController6.java
+++ src/main/java/com/example/spring/api/ApiController6.java
@@ -4,15 +4,13 @@ import com.example.spring.dto.a6.Customer;
 import com.example.spring.dto.a6.Supplier;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+//@Controller
+//@ResponseBody
+@RestController // Controller+ResponseBody
 @RequestMapping("/api/main6")
 @RequiredArgsConstructor
-@ResponseBody
 public class ApiController6 {
     private final Service6 service;
 

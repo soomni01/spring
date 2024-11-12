6ffcf76549d91cf7fc99b43141fcef4c461a31e5
soomni01 <dsgh8933@naver.com>
2024-11-12 12:40:11 +0900

    response body

--- src/main/java/com/example/spring/api/ApiController5.java
+++ src/main/java/com/example/spring/api/ApiController5.java
@@ -5,9 +5,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/api/main5")
 public class ApiController5 {
+
+    @GetMapping("sub7")
+    @ResponseBody
+    public Map<String, Object> sub7() {
+        return Map.of("product", Map.of("name", "kim", "price", 5000,
+                "location", List.of("seoul", "busan"), "quantity", 5));
+    }
+
+    @GetMapping("sub6")
+    @ResponseBody
+    public Map<String, Object> sub6() {
+        return Map.of("name", "son", "items", List.of("치킨", "돈까스", "떡볶이"),
+                "team", Map.of("name", "토트넘", "location", "런던"));
+    }
+
+    @GetMapping("sub5")
+    @ResponseBody
+    public Map<String, Object> sub5() {
+        return Map.of("title", "책제목", "content", "책 내용",
+                "price", 15000, "available", true);
+    }
+
+    @GetMapping("sub4")
+    @ResponseBody
+    public Map<String, Object> sub4() {
+        return Map.of("name", "kim", "age", 44);
+    }
+
+    @GetMapping("sub3")
+    @ResponseBody
+    public String method() {
+
+        return "seoul";
+    }
+
     @GetMapping("sub1")
     @ResponseBody
     public String method1() {

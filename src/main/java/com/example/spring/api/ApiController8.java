ff674f8e4efecffaabca2f65aaa947e180b66fcd
soomni01 <dsgh8933@naver.com>
2024-11-13 11:31:50 +0900

    파일 업로드

--- src/main/java/com/example/spring/api/ApiController8.java
+++ src/main/java/com/example/spring/api/ApiController8.java
@@ -3,10 +3,7 @@ package com.example.spring.api;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Map;
 
 @RestController
@@ -14,6 +11,13 @@ import java.util.Map;
 public class ApiController8 {
 
     @PostMapping("sub1")
+    // @RequestParam
+    // 클라이언트에서 보낸 폼 데이터 또는 쿼리 파라미터를 메소드 파라미터로 바인딩할 때 사용
+    //주로 GET 요청의 쿼리 파라미터 또는 POST 요청의 폼 데이터에서 값을 추출할 때 사용
+
+    // @RequestBody
+    // HTTP 요청의 **본문(body)**에 담긴 JSON, XML, 또는 기타 포맷의 데이터를 객체로 바인딩할 때 사용
+    // 주로 POST나 PUT 요청에서 본문에 데이터를 전송할 때 사용
     public void method1(@RequestParam String city,
                         @RequestParam String country,
 
@@ -45,5 +49,33 @@ public class ApiController8 {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @PostMapping("sub2")
+    public void method2(@RequestParam String title,
+                        @RequestParam MultipartFile attached) throws IOException {
+        System.out.println("title = " + title);
+        System.out.println("attached = " + attached.getOriginalFilename());
+
+        attached.transferTo(new File("C:/Temp/" + attached.getOriginalFilename()));
+//        try {
+//            InputStream is = attached.getInputStream();
+//            BufferedInputStream bis = new BufferedInputStream(is);
+//
+//            String outputFilePath = "C:/Temp/" + attached.getOriginalFilename();
+//            FileOutputStream fos = new FileOutputStream(outputFilePath);
+//            BufferedOutputStream bos = new BufferedOutputStream(fos);
+//
+//            try (bis; bos; fos; is) {
+//                byte[] buffer = new byte[1024];
+//                int length;
+//                while ((length = bis.read(buffer)) != -1) {
+//                    bos.write(buffer, 0, length);
+//                }
+//                bos.flush();
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }

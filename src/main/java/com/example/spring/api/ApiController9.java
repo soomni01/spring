d92e2f6f3a40e24ca0be5a3bf1ee3539e026c698
soomni01 <dsgh8933@naver.com>
2024-11-19 10:22:30 +0900

    권한 예제

--- src/main/java/com/example/spring/api/ApiController9.java
+++ src/main/java/com/example/spring/api/ApiController9.java
@@ -2,6 +2,7 @@ package com.example.spring.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.Collection;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,6 +19,15 @@ import java.time.Instant;
 public class ApiController9 {
 
     final JwtEncoder jwtEncoder;
+
+    @GetMapping("sub10")
+    public String sub10(Authentication authentication) {
+        String name = authentication.getName(); // 사용자 이름
+        Collection authorities = authentication.getAuthorities();// 권한 목록
+        System.out.println("name = " + name);
+        System.out.println("authorities = " + authorities);
+        return null;
+    }
 
     @GetMapping("sub9")
     @PreAuthorize("hasAnyAuthority('SCOPE_admin', 'SCOPE_manager')")
